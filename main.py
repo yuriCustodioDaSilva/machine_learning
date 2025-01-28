82faf74e01fabfe3843d75517715dca691be3373
Yuri <yuri.custodio.silva@gmail.com>
2025-01-28 20:43:37 -0300

    trying to get me back to a workout base on body part

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import NearestNeighbors
 
-df = pd.read_csv("pizza.csv")
+df = pd.read_csv('megaGymDataset.csv')
+df = df.dropna(subset=['BodyPart', 'Rating','Desc'])
 
-print(df)
+le_bodypart = LabelEncoder()
+df['BodyPart_encoded'] = le_bodypart.fit_transform(df['BodyPart'])
+
+le_equipment = LabelEncoder()
+df['Equipment_encoded'] = le_equipment.fit_transform(df['Equipment'])
+
+X = df[['BodyPart_encoded', 'Equipment_encoded', 'Rating']]
+
+knn = NearestNeighbors(n_neighbors=5, algorithm='auto')
+knn.fit(X)
+
+user_input = 'Chest' 
+user_input_encoded = le_bodypart.transform([user_input])[0]
+
+distances, indices = knn.kneighbors([[user_input_encoded, 0, 0]]) 
+
+recommended_exercises = df.iloc[indices[0]]
+
+# print(recommended_exercises[['Title', 'Rating','Desc','BodyPart']])
+
+recommended_exercises[['Title', 'Rating', 'BodyPart']].to_excel('recommended_exercises.xlsx', index=False)
+
+print("Recommendations have been saved to 'recommended_exercises.xlsx'.")
